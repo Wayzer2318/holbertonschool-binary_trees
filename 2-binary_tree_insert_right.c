d18850f5e9ef7b02797fe4e8d8fce00b1cf29cf5
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-11 01:29:38 +0100

    adding the binary_tree_insert_right.c file

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - function that insert on the right
+ * @value: the value of the node
+ * @parent: the parent of the node
+ * Return: newnode
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t newnode;
+
+	if (parent == NULL)
+		return (NULL);
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL)
+		return (NULL);
+
+	newnode->parent = parent;
+	newnode->n = value;
+	newnode->right = NULL;
+	newnode->left = NULL;
+
+	if (parent->right != NULL)
+	{
+		newnode->right = parent->right;
+		parent->right->parent = newnode;
+	}
+	parent->right = newnode;
+	
+
+	retrun (newnode);
+}
