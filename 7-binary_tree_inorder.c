022bb90bc888f7187e1c1548f036d7f2e2073ac9
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-11 22:26:04 +0100

    adding the binary_tree_inorder file

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * bianry_tree_inorder - inorder the tree
+ * @tree: tree to inorder
+ * @func: function to execute
+ * Return: nothing
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left,func);
+		func(tree->n);
+		binary_tree_inorder(tree->right,func);
+	}
+}
