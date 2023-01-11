28c9659c58bb993984c8968d950e3679853e71a4
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-11 22:18:02 +0100

    adding description to the binary_tree_preorder file

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ * binary_tree_preorder - preorder the tree
+ * @tree: tree to order
+ * @func: function to execute
+ * Return: nothing
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
