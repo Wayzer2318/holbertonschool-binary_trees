d50aa55f2869ef627fefc88979fb57c364403e0c
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-11 01:14:46 +0100

    fixed bug on the binary_tree_insert_left.c file

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,23 +1,24 @@
 #include "binary_trees.h"
 /**
  * binary_tree_insert_left - fuction that insert a new node at the left
- *
- *
+ * @value: value of node
+ * @parent: parent of the node
+ * Return: new node on the left of parent
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newnode;
 
 	newnode = malloc(sizeof(binary_tree_t));
-	if(!newnode)
+	if (!newnode)
 		return (NULL);
-	if(!parent)
+	if (!parent)
 		return (NULL);
 	newnode->parent = parent;
 	newnode->n = value;
 	newnode->right = NULL;
 	newnode->left = NULL;
-	
+
 	if (parent->left != NULL)
 	{
 		newnode->left = parent->left;
