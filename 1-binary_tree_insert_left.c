99f9b873c11cb88dbcd6f51efadfbbc84959f99e
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-11 01:03:38 +0100

    adding the binary tree left insertion file

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - fuction that insert a new node at the left
+ *
+ *
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if(!newnode)
+		return (NULL);
+	if(!parent)
+		return (NULL);
+	newnode->parent = parent;
+	newnode->n = value;
+	parent->left = newnode;
+}
