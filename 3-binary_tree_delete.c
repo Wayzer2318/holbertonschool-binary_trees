e1d97ed7cbb255d987cb1722e8fa2088017e4da0
you-tech-sys <62576535+you-tech-sys@users.noreply.github.com>
2023-01-11 01:47:19 +0100

    adding the binary_tree_delete file

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,8 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ * binary_tree_delete - delete a tree
+ * @tree: tree to be deleted
+ * Return: nothing
  */
 void binary_tree_delete(binary_tree_t *tree)
 {	
