62bbdb2ea9b83cd2eb977b69b708046d34740536
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-12 21:34:32 +0100

    fixed bug for binary tree height file

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,8 +8,8 @@
 {
     if (tree == NULL)
         return (0);
-    size_t leftHeight = binary_tree_height(tree->left);
-    size_t rightHeight = binary_tree_height(tree->right);
+    binary_tree_t leftHeight = binary_tree_height(tree->left);
+    binary_tree_t rightHeight = binary_tree_height(tree->right);
     if (leftHeight >= rightHeight)
         return (leftHeight + 1);
     else
