5a800c1f9ed9217b45b415864fa45955b31567e7
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-12 21:49:03 +0100

    definitly fixed bug

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,9 +1,8 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
- *
+ * binary_tree_height - function return height of tree
+ * @tree: Pointer to the root of tree
+ * Return: value of height
 */size_t binary_tree_height(const binary_tree_t *tree)
 {
     size_t heightLeft = 0, heightRight = 0;
