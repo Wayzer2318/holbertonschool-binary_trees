8c0659f62c79ae86e7b33fcfa88a9c51b3669e08
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-13 20:16:29 +0100

    adding the binary tree balence file

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+
+
+/**
+ * binary_tree_height - function return height of tree
+ * @tree: Pointer to the root of tree
+ * Return: value of height
+ *
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t heightLeft = 0, heightRight = 0;
+
+	if (!tree)
+		return (0);
+
+	heightLeft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	heightRight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+
+return (heightRight >= heightLeft ? heightRight : heightLeft);
+
+
+}
