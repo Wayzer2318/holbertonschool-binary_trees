82df48d3896bba5a41f31289fd863c36b08ce8f9
Wayzer2318 <arezki.ait.kettout@gmail.com>
2023-01-13 15:57:40 +0100

    fixed bug for the binary tree depth file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -6,16 +6,14 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	else
-	{
-		int ldepth = binary_tree_depth(tree->left);
-		int rdepth = binary_tree_depth(tree->right);
+	size_t		r;
+	binary_tree_t	*p;
 
-		if(ldepth > rdepth)
-			return (ldepth + 1);
-		else
-			return (rdepth + 1);
+	r = 0;
+	p = (binary_tree_t *) (void *) tree;
+	while (p && p->parent)
+	{
+		p = p->parent;
+		r++;
 	}
-}
+	return (r);}
